f3726c49f3950608ce70ef70b1796ae99d75f316
VINCENTCHANPUIWAI <94903334+VINCENTCHANPUIWAI@users.noreply.github.com>
2022-05-17 00:56:30 +0800

    up 2

--- Fruit_Scanner/ContentView.swift
+++ Fruit_Scanner/ContentView.swift
@@ -21,7 +21,7 @@ struct ContentView: View {
         .navigationBarHidden(true)
         .navigationTitle("")
         .onAppear{
-            FirebaseApp.configure()
+          
         }
     }
 }
--- Fruit_Scanner/Fruit_ScannerApp.swift
+++ Fruit_Scanner/Fruit_ScannerApp.swift
@@ -3,10 +3,15 @@
 //  Fruit_Scanner
 
 import SwiftUI
+import Firebase
 
 @main
 struct Fruit_ScannerApp: App {
     let persistenceController = PersistenceController.shared
+    
+    init() {
+        FirebaseApp.configure()
+    }
 
     var body: some Scene {
         WindowGroup {
